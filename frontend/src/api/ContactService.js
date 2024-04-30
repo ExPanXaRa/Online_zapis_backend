f4970d0aa3ccd7795ee3b34ff211f38ed83a6db8
ExPanXaRa <aleks.ru.6@yandex.ru>
2024-04-30 05:40:11 +0300

    a

--- frontend/src/api/ContactService.js
+++ frontend/src/api/ContactService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/contacts';
+const API_URL = 'http://backend:8080/contacts';
 
 export async function saveContact(contact) {
     return await axios.post(API_URL, contact);
